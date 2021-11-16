843de66550eb6ffddf513177e18cbd41f65a5db4
GilbertoConchaE <90362089+GilbertoConchaE@users.noreply.github.com>
2021-11-15 21:21:26 -0500

    Add files via upload
    The application must read the POI information from a local file in json format.
    
    • The following information should be displayed: name or title, 
    general information, location, temperature and recommended sites, etc.
    • User interface tests must be created to validate operation.
    • The information required is save in two files: PoiFirstSprint-10thGroup.js and PoiFirstSprint-10thGroup.xml

---
+++ PoiFirstSprint-10thGroup.js
@@ -0,0 +1,31 @@
+[{
+		"poiName": "Museo del Señor de los Milagros",
+		"poiGeneralInfo": "Conserva una muestra de los elementos que han hecho parte de la historia del Señor de los Milagros de Buga",
+		"poiLocation": "Carrera 14 #3-37, Guadalajara de Buga",
+		"poiTemperature": "30°C"
+	},
+	{
+		"poiName": "Museo de arte religioso",
+		"poiGeneralInfo": "Expone retablos, santos, textos y mobiliario usado en actividades eclesiasticas de índole católico",
+		"poiLocation": "Carrera 9 #3 - 55, Guacarí",
+		"poiTemperature": "29°C"
+	},
+	{
+		"poiName": "Museo Rayo",
+		"poiGeneralInfo": "Expone caricaturas, acuarelas, pasteles, plumillas e intaglios elaborados por el Maestro Omar Rayo.",
+		"poiLocation": "Calle 8 #8-53, Roldanillo",
+		"poiTemperature": "27°C"
+	},
+	{
+		"poiName": "Museo de Arte Moderno La Tertulia",
+		"poiGeneralInfo": "Exibición permanente de obras de artes plásticas, cine, arquitectura y fotografía",
+		"poiLocation": "Avenida Colombia No. 5-105 Oeste, Santiago de Cali",
+		"poiTemperature": "28°C"
+	},
+	{
+		"poiName": "Museo de la Caña de Azucar y Hacienda Piedechinche",
+		"poiGeneralInfo": "Distintos senderos muestran la historia de los trapiches y consrucciones típicas de distintas zonas de la geografía colombiana",
+		"poiLocation": "Localizado en la hacienda Piedechince, Corregimiento Santa Elena, vereda Amaimito en el municipio de El Cerrito, a unos 42 kilómetros de Cali",
+		"poiTemperature": "21°C"
+	}
+]
